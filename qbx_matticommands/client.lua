90f85a35518d5c27625f0bd968d6cdb434bf6da5
Matti <57048210+MattiVboiii@users.noreply.github.com>
2024-10-08 19:38:45 +0200

    Delete qbx_matticommands/client.lua
    don't need this with my new updates :)

--- qbx_matticommands/client.lua
+++
@@ -1,5 +0,0 @@
-RegisterNetEvent('qbx_core:client:Teleport')
-AddEventHandler('qbx_core:client:Teleport', function(coords)
-    local playerPed = PlayerPedId()
-    SetEntityCoords(playerPed, coords.x, coords.y, coords.z, false, false, false, true)
-end)
